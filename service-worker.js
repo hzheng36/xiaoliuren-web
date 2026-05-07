2dd6672317de9b40dca3e73f0c7593ef51aa2184
hzheng36 <hzheng36@gmail.com>
2026-05-06 22:56:37 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,16 @@
-const CACHE_NAME = 'xiaoliuren-v9_9_28';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './apple-touch-icon.png',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const CACHE_NAME = 'xiaoliuren-v10-0';
+const FILES_TO_CACHE = ['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => {
-      if (key !== CACHE_NAME && key.startsWith('xiaoliuren-')) return caches.delete(key);
-    }))).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME && k.startsWith('xiaoliuren-')).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
-  if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-        return res;
-      }).catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-      return res;
-    }).catch(() => cached))
-  );
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return resp;
+  }).catch(() => caches.match('./index.html'))));
 });
