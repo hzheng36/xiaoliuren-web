3c8ccd2048d08d3059802697000599ab6ee45b1a
hzheng36 <hzheng36@gmail.com>
2026-05-09 20:56:02 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaoliuren-v10.5.38-nav-isolation-terms';
+const CACHE_NAME = 'xiaoliuren-v10.5.39-destiny-terms-collapse-guard';
 const APP_SHELL = [
   './',
   './index.html',
