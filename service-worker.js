640bb6e73542936f9d8d98631864feb525fdb354
hzheng36 <hzheng36@gmail.com>
2026-05-10 23:45:30 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaoliuren-v10.5.103-night-residue-mainline-clean';
+const CACHE_NAME = 'xiaoliuren-v10.5.104-night-history-mainline-fix';
 const APP_SHELL = [
   './',
   './index.html',
