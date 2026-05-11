5a9ddf7b03238e28a627fda5a44905a7ca88390a
hzheng36 <hzheng36@gmail.com>
2026-05-11 01:07:59 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaoliuren-v10-5-110-destiny-night-paipan-trim';
+const CACHE_NAME = 'xiaoliuren-v10-5-111-destiny-subnav-night-check';
 const APP_SHELL = [
   './',
   './index.html',
